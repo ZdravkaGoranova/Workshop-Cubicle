086b39acca4487f6420fbdc98fc466d0ea28832d
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-01-20 13:13:15 +0200

    Setup dev environment

--- src/config.js
+++ src/config.js
@@ -6,6 +6,8 @@ const config = {
         PORT: 5000,
     }
 };
-module.exports = config[process.env.node_env || 'development'];//искам да експоернтна определена част, която отговаря на текущата process.env  променлива 
+module.exports = config[process.env.node_env || 'development'];
+//process.env.node_env.trim() и в package.json  "start": " SET node_env=production &&node ./src/index.js",
+//искам да експоернтна определена част, която отговаря на текущата process.env  променлива 
 //ако не е сетната използвай development
 //според средата на изпълнение
