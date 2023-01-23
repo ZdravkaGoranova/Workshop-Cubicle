b53cfe464fe336cb0986bfaedbf0b61dfd4c612a
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-01-24 01:53:39 +0200

    remove static

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -10,7 +10,7 @@ exports.postCreateCube = (req, res) => {
     //save cube
     const { name, description, imageUrl, difficultyLevel } = req.body
     let cube = new Cube(name, description, imageUrl, difficultyLevel);
-    Cube.save(cube);
+    cube.save();
 
     //redirect
     res.redirect('/');
--- src/models/Cube.js
+++ src/models/Cube.js
@@ -9,9 +9,9 @@ class Cube {
         this.imageUrl = imageUrl;
         this.difficultyLevel = difficultyLevel;
     }
-    static save(cube) {
-        cube.id = db.cubes[db.cubes.length - 1].id + 1;
-        db.cubes.push(cube);
+     save() {
+        this.id = db.cubes[db.cubes.length - 1].id + 1;
+        db.cubes.push(this);
         const jsonData = JSON.stringify(db, null, 2);
         fs.writeFileSync(path.resolve(__dirname, '../db.json'), jsonData);
     }
