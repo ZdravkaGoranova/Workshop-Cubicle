e43d66d643c1c55feb9c12a32206c1e71c843a49
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-02-05 20:32:46 +0200

    add cookieParser

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 console.log("Starting my workshop :)");
 
 const express = require('express');
+const cookieParser = require('cookie-parser');
+
 const routes = require('./routes.js');
 const config = require('./config');//Ако файла се казва различно име от index.js, примерно config.js->require('./config/config.js');
 const setupViewEngine = require('./config/viewEngine.js');
@@ -12,12 +14,13 @@ setupViewEngine(app);
 // или require('./config/viewEngine.js')(app);
 
 app.use(express.static('src/public'));//проверяваме дали работи в browser http://localhost:5000/css/site.css
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));//връща middleware,който ни парсва urlcoded bodies 
 //прочита данните от req и ще ги парсва за всеки req;
 app.use(routes);//за всички заявки използвай този router
 
 initDataBase()
-.then(()=>app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`)))
-//ако не работи базата данни да не се стартира приложението
-.catch((err)=>console.error(err.message));
+    .then(() => app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`)))
+    //ако не работи базата данни да не се стартира приложението
+    .catch((err) => console.error(err.message));
 
